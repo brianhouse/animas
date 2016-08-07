fixed normalization issue

reduce.py
@@ -27,25 +27,24 @@ labels += abs(min(labels))
 max_label = max(labels)
 log.info("CENTROIDS\n%s" % centroids)
 
-cxs = sp.normalize(centroids[:,0], min(points[:,0]), max(points[:,0]))
-cys = sp.normalize(centroids[:,1], min(points[:,1]), max(points[:,1]))
+centroids = np.column_stack((sp.normalize(centroids[:,0], np.min(points[:,0]), np.max(points[:,0])), sp.normalize(centroids[:,1], np.min(points[:,1]), np.max(points[:,1]))))
+points = np.column_stack((sp.normalize(points[:,0], np.min(points[:,0]), np.max(points[:,0])), sp.normalize(points[:,1], np.min(points[:,1]), np.max(points[:,1]))))
 
-xs = sp.normalize(points[:,0])
-ys = sp.normalize(points[:,1])
 
 ctx = drawing.Context(500, 500)#, hsv=True)
 
-for i in range(len(xs)):
-    pos = i / len(xs)
-    # ctx.arc(xs[i], ys[i], 2/ctx.width, 2/ctx.height, thickness=0.0, fill=(0.35 + (pos * 0.65), 1.0, 1.0))
-    ctx.arc(xs[i], ys[i], 2/ctx.width, 2/ctx.height, thickness=0.0, fill=colors[labels[i]])
-    # if i == 0:
-    #     continue
-    # ctx.line(xs[i], ys[i], xs[i-1], ys[i-1], stroke=(0.35 + (pos * 0.65), 1.0, 1.0))
+for i, point in enumerate(points):
+    pos = i / len(points)
+    # ctx.arc(point[0], point[1], 2/ctx.width, 2/ctx.height, thickness=0.0, fill=(0.35 + (pos * 0.65), 1.0, 1.0))
+    ctx.arc(point[0], point[1], 2/ctx.width, 2/ctx.height, thickness=0.0, fill=colors[labels[i]])
+    if i == 0:
+        continue
+    prev = points[i-1]
+    # ctx.line(point[0], point[1], prev[0], prev[1], stroke=(0.35 + (pos * 0.65), 1.0, 1.0))
 
 # yeah, see -- how can you reduce it now?
-for i in range(len(cxs)):
-    ctx.arc(cxs[i], cys[i], 2/ctx.width, 2/ctx.height, thickness=5.0)
+for centroid in centroids:
+    ctx.arc(centroid[0], centroid[1], 2/ctx.width, 2/ctx.height, thickness=5.0)
 
 # for i in range(100):
 #     pos = i / 100