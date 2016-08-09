19cdb40453ea2cff1f645a409533d5d7d9ffe4f0
brianhouse <brian.house@gmail.com>
2016-08-09 12:51:11 -0400

    implemented with matplotlib

--- query.py
+++ query.py
@@ -3,7 +3,7 @@
 import json
 import numpy as np
 import signal_processing as sp
-from housepy import config, log, util, drawing
+from housepy import config, log, util, drawing, strings
 from mongo import db
 from colors import colors
 
@@ -20,7 +20,8 @@ start = "2016-04-06"
 # start = "2016-04-15"
 # stop = "2016-05-30"
 
-start = "2016-08-01"
+start = "2016-07-07"
+# start = "2016-08-01"
 stop = "2016-08-07"
 
 print(config['sites'][site])
@@ -31,7 +32,10 @@ results = list(results)
 print(json.dumps(results[0], indent=4, default=lambda d: str(d)))
 
 ts = [d['t_utc'] for d in results]
+duration = ts[-1] - ts[0]
+print("DURATION %s %s" % (duration, strings.format_time(duration)))
 signals = []
+rates = []
 labels = list(config['labels'].values())
 labels.sort()
 for i, label in enumerate(labels):
@@ -40,6 +44,9 @@ for i, label in enumerate(labels):
         values = [d[label] if label in d else None for d in results]
         values = sp.remove_shots(values, nones=True)  # repair missing values
         signal = sp.resample(ts, values)
+        num_samples = len(signal)
+        sample_rate = num_samples / duration
+        rates.append(sample_rate)
         signal = sp.normalize(signal)
         signal = sp.smooth(signal, 15)
         signals.append(signal)        
@@ -60,4 +67,5 @@ for i in range(len(signals[0])):
     points.append(point)
 
 points = np.array(points)
-util.save("data/%s.pkl" % util.timestamp(), points)
+util.save("data/%s.pkl" % util.timestamp(), (points, rates))
+util.save("data/last.pkl", (points, rates))
--- reduce.py
+++ reduce.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
-import json
+import json, time
 import numpy as np
 import signal_processing as sp
 from sklearn import manifold, decomposition, cluster
-from housepy import config, log, util, drawing
-from colors import colors
+from housepy import config, log, util, chart
 
-# points = util.load("data/1470432105.pkl")   # last week
-points = util.load("data/1470593687.pkl")   # last two weeks
+# points, rates = util.load("data/1470432105.pkl")   # last week
+# points, rates = util.load("data/1470593687.pkl")   # last two weeks
+# points, rates = util.load("data/1470681705.pkl")   # smoothed!
+# points, rates = util.load("data/1470681860.pkl")   # not smoothed!
+points, rates = util.load("data/last.pkl")
 log.info("INPUT: %s POINTS, %s DIMENSIONS" % points.shape)
 # points = manifold.Isomap().fit_transform(points)
 # points = manifold.LocallyLinearEmbedding(method="modified").fit_transform(points)
@@ -32,25 +34,9 @@ centroids = np.column_stack((sp.normalize(centroids[:,0], np.min(points[:,0]), n
 points = np.column_stack((sp.normalize(points[:,0], np.min(points[:,0]), np.max(points[:,0])), sp.normalize(points[:,1], np.min(points[:,1]), np.max(points[:,1]))))
 
 
-ctx = drawing.Context(500, 500, hsv=True)
 
-for i, point in enumerate(points):
-    pos = i / len(points)
-    ctx.arc(point[0], point[1], 2/ctx.width, 2/ctx.height, thickness=0.0, fill=(0.35 + (pos * 0.65), 1.0, 1.0))
-    # ctx.arc(point[0], point[1], 2/ctx.width, 2/ctx.height, thickness=0.0, fill=colors[labels[i]])
-    if i == 0:
-        continue
-    prev = points[i-1]
-    # ctx.line(point[0], point[1], prev[0], prev[1], stroke=(0.35 + (pos * 0.65), 1.0, 1.0))
+chart.plot(points, sample_axis=True, scatter=False, c=(0., 0., 1., 1.), linewidth=2)
+chart.plot(centroids, sample_axis=True, scatter=True, c=(1., 0., 0., 1.), linewidth=0, s=100)
 
-# yeah, see -- how can you reduce it now?
-for centroid in centroids:
-    ctx.arc(centroid[0], centroid[1], 2/ctx.width, 2/ctx.height, thickness=5.0)
-
-for i in range(100):
-    pos = i / 100
-    ctx.arc(pos, 20 / ctx.width, 3/ctx.width, 3/ctx.height, thickness=0.0, fill=(0.35 + (pos * 0.65), 1.0, 1.0))
-
-ctx.output("charts/")
-
+chart.show("charts/")
 
